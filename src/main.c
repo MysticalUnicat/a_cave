733baef5df409c1d3d3a65b71f9bdf5fc9dced6d
Sarah Burns <mystical.unicat@gmail.com>
2021-04-28 18:12:26 -0700

    auto

--- src/main.c
+++ src/main.c
@@ -1,17 +1,35 @@
 #include <alias/ecs.h>
 #include <raylib.h>
 
-#define DEFINE_FONT(IDENT, PATH) \
-  Font * IDENT () {              \
-    static Font font, * ptr = NULL; \
-    if(ptr == NULL) {                  \
-      font = LoadFont(PATH);     \
-      ptr = &font; \
-    }                            \
-    return ptr;                 \
+#define LAZY_GLOBAL(TYPE, IDENT, CODE) \
+  TYPE IDENT (void) {                  \
+    static TYPE inner;                 \
+    static int init = 0;               \
+    if(init == 0) {                    \
+      CODE                             \
+      init = 1;                        \
+    }                                  \
+    return inner;                      \
   }
 
-DEFINE_FONT(Romulus, "resources/fonts/romulus.png");
+#define LAZY_GLOBAL_PTR(TYPE, IDENT, CODE) \
+  TYPE * IDENT (void) {                    \
+    static TYPE inner;                     \
+    static TYPE * __ptr = NULL;            \
+    if(__ptr == NULL) {                    \
+      CODE                                 \
+      __ptr = &inner;                      \
+    }                                      \
+    return __ptr;                          \
+  }
+
+#define DEFINE_FONT(IDENT, PATH) LAZY_GLOBAL_PTR(Font, IDENT, inner = LoadFont(PATH);)
+
+#define DEFINE_WORLD(IDENT) LAZY_GLOBAL(alias_ecs_Instance *, IDENT, alias_ecs_create_instance(NULL, &inner);)
+
+DEFINE_FONT(Romulus, "resources/fonts/romulus.png")
+
+DEFINE_WORLD(World)
 
 int main(int argc, char * argv []) {
   int screen_width = 800;
