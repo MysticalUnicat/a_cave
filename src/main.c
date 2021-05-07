a280d67e30a4153fbb4ecd7afbf8f486c67599fd
Sarah Burns <mystical.unicat@gmail.com>
2021-05-07 14:31:16 -0700

    auto Fri May  7 02:31:16 PM PDT 2021

--- src/main.c
+++ src/main.c
@@ -5,13 +5,60 @@ DEFINE_FONT(Romulus, "resources/fonts/romulus.png")
 DEFINE_WORLD(World)
 
 DEFINE_COMPONENT(World(), Position, {
-  float x;
-  float y;
+  Vector2 pos;
+})
+
+DEFINE_COMPONENT(World(), AnimatePosition, {
+  Vector2 from;
+  Vector2 to;
+  float start;
+  float end;
 })
 
 DEFINE_COMPONENT(World(), Text, {
   const char * text;
+  Font * font;
+  float size;
+  Color color;
 });
+
+#define GRID_SIZE 32
+
+static inline float to_grid(float x) {
+  return floor(x / (float)GRID_SIZE) * (float)GRID_SIZE;
+}
+
+static inline void simulate(void) {
+  //ECS(begin_recording, World(), cbuf);
+  float time = GetTime();
+
+  QUERY(World(), ( write, Position, pos ), ( read, AnimatePosition, ani )) {
+    float t = (time - ani->start) / (ani->end - ani->end);
+    if(time > ani->end) {
+      pos->pos = ani->to;
+      //ECS(cmd_remove_component, cbuf, entity, AnimatePosition_component());
+      return;
+    }
+    
+    pos->pos.x = ani->from.x + (ani->to.x - ani->from.x) * t;
+    pos->pos.y = ani->from.y + (ani->to.y - ani->from.y) * t;
+  }
+
+  //ECS(end_recording, World(), cbuf);
+  //ECS(execute_commands, World(), 1, &cbuf);
+}
+
+static inline void draw(void) {
+  BeginDrawing();
+
+  ClearBackground(BLACK);
+
+  QUERY(World(), ( read, Position, position ), ( read, Text, text )) {
+    DrawTextEx(*text->font, text->text, position->pos, text->font->baseSize * text->size, 3, text->color);
+  }
+
+  EndDrawing();
+}
 
 int main(int argc, char * argv []) {
   int screen_width = 800;
@@ -23,24 +70,16 @@ int main(int argc, char * argv []) {
 
   SPAWN(
     World(),
-    ( Position, .x = 0.0f, .y = 0.0f ),
-    (     Text, .text = "A CAVE"     )
+    ( Position, .pos = (Vector2) { .x = 0.0f, .y = 0.0f }  ),
+    (     Text, .text = "A CAVE", .font = Romulus(), .size = 2.0f, .color = DARKPURPLE )
   );
 
+  //alias_ecs_CommandBuffer cbuf;
+  //ECS(create_command_buffer, World(), &cbuf);
+
   while(!WindowShouldClose()) {
-    BeginDrawing();
-
-    ClearBackground(BLACK);
-
-    QUERY(World(), ( read, Position, position ), ( read, Text, text )) {
-      DrawTextEx(*Romulus(), text->text, (Vector2) { .x = position->x, .y = position->y }, Romulus()->baseSize * 2.0f, 3, DARKPURPLE);
-    }
-
-    QUERY(World(), ( write, Position, position )) {
-      position->x += GetFrameTime() * 10;
-    }
-
-    EndDrawing();
+    simulate();
+    draw();
   }
 
   CloseWindow();
