ff44381e5ece0b4189c1dd636a776add524862b3
Sarah Burns <mystical.unicat@gmail.com>
2021-05-13 01:33:53 -0700

    auto Thu May 13 01:33:53 AM PDT 2021

--- src/event.h
+++ src/event.h
@@ -8,5 +8,15 @@ DECLARE_COMPONENT(Event, {
 
 #define SPAWN_EVENT(...) SPAWN(( Event, .id = event_id() ), ## __VA_ARGS__)
 
+#define EVENT_QUERY(...) \
+  static uint32_t CAT(_leid, __LINE__) = 0; \
+  uint32_t CAT(_neid, __LINE__) = CAT(_leid, __LINE__); \
+  _QUERY( \
+    if(_event->id <= CAT(_leid, __LINE__)) return; \
+    if(_event->id > CAT(_neid, __LINE__)) CAT(_neid, __LINE__) = _event->id; \
+  , \
+    CAT(_leid, __LINE__) = CAT(_neid, __LINE__); \
+  , ( read, Event, _event ), ## __VA_ARGS__)
+
 uint32_t event_id(void);
 void event_update(void);
