a6d83e9652b6507cd9a5bf8443b3cf3f273dbf39
Sarah Burns <mystical.unicat@gmail.com>
2021-05-26 21:54:12 -0700

    auto Wed May 26 09:54:12 PM PDT 2021

--- src/render.c
+++ src/render.c
@@ -16,7 +16,29 @@ void render_frame(void) {
       ( read, alias_LocalToWorld2D, t )
     , ( read, DrawRectangle, r )
   ) {
-    DrawRectangle(t->value._13 - r->width / 2.0f, t->value._23 - r->height / 2.0f, r->width, r->height, r->color);
+    alias_R
+        hw = r->width / 2
+      , hh = r->height / 2
+      , bl = -hw
+      , br =  hw
+      , bt = -hh
+      , bb =  hh
+      ;
+
+    alias_Point2D box[] = {
+        { br, bb }
+      , { br, bt }
+      , { bl, bt }
+      , { bl, bb }
+      };
+
+    box[0] = alias_multiply_Affine2D_Point2D(t->value, box[0]);
+    box[1] = alias_multiply_Affine2D_Point2D(t->value, box[1]);
+    box[2] = alias_multiply_Affine2D_Point2D(t->value, box[2]);
+    box[3] = alias_multiply_Affine2D_Point2D(t->value, box[3]);
+
+    DrawTriangle(*(Vector2*)&box[0], *(Vector2*)&box[1], *(Vector2*)&box[2], r->color);
+    DrawTriangle(*(Vector2*)&box[0], *(Vector2*)&box[2], *(Vector2*)&box[3], r->color);
   }
 
   QUERY(
